fb8987aac582ba4aa45a6b67cfd276bfba920721
Danielle Grogan <dgrogan@squam.sr.unh.edu>
2021-02-26 16:17:49 -0500

    added figures to output

--- src/main/spinup_clim_aggregation.R
+++ src/main/spinup_clim_aggregation.R
@@ -6,6 +6,7 @@ library(raster)
 rasterOptions(tmpdir = "/net/usr/spool/")   # set alternative /tmp directory
 library(rgdal)
 library(rgeos)
+library(zoo)
 
 ### Source functions from other github repos:
 # wbm_load()
@@ -28,6 +29,8 @@ source("src/functions/extract_ts.R")
 # paths
 path.base = "/net/nfs/squam/raid/data/WBM_TrANS/HiMAT/2021-02/HiMAT_clim_1981_2010/"  #NB: year 2010 got cut off - don't use
 path.out = "results/spinup_clim/"
+path.base = "/net/nfs/squam/raid/data/WBM_TrANS/HiMAT/2021-02/HiMAT_wbm_core_noCrops/"  #NB: year 2010 got cut off - don't use
+
 
 # shapefiles for spatial agg
 basins = readOGR("data/basins_hma", "basins_hma")  # basins to aggregate over
@@ -98,23 +101,34 @@ subbasin.agg = lapply(vars.stor, function(var) extract_ts(raster.path = file.pat
 
 # YEARLY
 # aggregate storage vars by basin
+
+# grdWater
+var = "grdWater"
 grdWater_yearly = 
   extract_ts(raster.path = file.path(path.base, "yearly", "grdWater"), 
              shp = basins, 
-             years = seq(1981, 2009), 
+             years = seq(1981, 1992), 
              var = "grdWater", 
              row.nm = as.character(basins$name),
-             out.nm = paste(path.out, var, "/basin_", var, "_km3_yearly.csv", sep=""),
+             out.nm = paste(path.out, var, "/pristine_basin_", var, "_km3_yearly.csv", sep=""),
              check.file = 0)
 
-ymin = min(as.numeric(grdWater_yearly[,2:30]))
-ymax = max(as.numeric(grdWater_yearly[,2:30]))
+ymin = min(as.numeric(grdWater_yearly[,2:ncol(grdWater_yearly)]))
+ymax = max(as.numeric(grdWater_yearly[,2:ncol(grdWater_yearly)]))
+
+for(i in 1:16){
+  #png(paste("figures/spinup_clim_figures/grdWater_avekm3Yr_", grdWater_yearly[i,1], ".png", sep=""))
+  plot(as.numeric(grdWater_yearly[i,2:ncol(grdWater_yearly)]), type='l',
+       main=paste(grdWater_yearly[i,1], "ave grdWater"), ylab = c("km3/yr"), xlab = c("Years from start"))
+  points(as.numeric(grdWater_yearly[i,2:ncol(grdWater_yearly)]))
+  #dev.off()
+}
 
 for(i in 1:16){
   if(i==1){
-    plot(as.numeric(grdWater_yearly[i,2:30]), type='l', ylim = c(ymin, ymax))
+    plot(as.numeric(grdWater_yearly[i,2:29]), type='l', ylim = c(ymin, ymax))
   }
-  lines(as.numeric(grdWater_yearly[i,2:30]), col=i)
+  lines(as.numeric(grdWater_yearly[i,2:29]), col=i)
 }
 
 # percent change
@@ -127,20 +141,136 @@ per.chg = function(vec.x){
 }
 
 for(i in 1:16){
-  p.change.1 = per.chg(as.numeric(grdWater_yearly[1, 2:30]))
-  plot(p.change.1, type='l', main = grdWater_yearly[i,1])
-  abline(h=0, col='red')
+  p.change.1 = per.chg(as.numeric(grdWater_yearly[i, 2:ncol(grdWater_yearly)]))
+  if(i == 1){
+    plot(p.change.1, type='l', ylim=c(-1.5,1.5), main = paste(grdWater_yearly[i,1], "% chg grdWater"))
+  }else{  
+    lines(p.change.1, col=i)
+  }
+  abline(h=0, col='black', lty=2)
+}
+
+for(i in 1:16){
+  p.change.1 = per.chg(as.numeric(grdWater_yearly[i, 2:ncol(grdWater_yearly)]))
+  
+  #png(paste("figures/spinup_clim_figures/grdWater_percent_chg_", grdWater_yearly[i,1], ".png", sep=""))
+  plot(p.change.1, type='l', ylim=c(-1.5,1.5), 
+       main=paste(grdWater_yearly[i,1], "grdWater"), ylab = c("percent change"), xlab = c("Years from start"))
+  abline(h=0, col='grey')
+  #abline(lm(p.change.1~seq(1:27)), col='red', lty=2)
+  #lines(rollmean(p.change.1, 5) ~ seq(5,26), col='blue', lwd=2)
+  #legend("topleft", legend=c("% change from previous year", "5-year rolling mean % chg"), 
+   #      lty=c(1,1), col=c('black', 'blue'), lwd=c(1,2))
+  #dev.off()
 }
 
 
+# soilMoist
+var="soilMoist"
+soilMoist_yearly = 
+  extract_ts(raster.path = file.path(path.base, "yearly", "soilMoist"), 
+             shp = basins, 
+             years = seq(1981, 2009), 
+             var = "soilMoist", 
+             row.nm = as.character(basins$name),
+             out.nm = paste(path.out, var, "/basin_", var, "_km3_yearly.csv", sep=""),
+             check.file = 0)
+
+for(i in 1:16){
+  png(paste("figures/spinup_clim_figures/soilMoist_avekm3Yr_", soilMoist_yearly[i,1], ".png", sep=""))
+  plot(as.numeric(soilMoist_yearly[i,2:29]), type='l',
+       main=paste(soilMoist_yearly[i,1], "ave soilMoist"), ylab = c("km3/yr"), xlab = c("Years from start"))
+  dev.off()
+}
+
+ymin = min(as.numeric(soilMoist_yearly[,2:29]))
+ymax = max(as.numeric(soilMoist_yearly[,2:29]))
+
+for(i in 1:16){
+  if(i==1){
+    plot(as.numeric(soilMoist_yearly[i,2:29]), type='l', ylim = c(ymin, ymax))
+  }
+  lines(as.numeric(soilMoist_yearly[i,2:29]), col=i)
+}
+
+# percent change
+per.chg = function(vec.x){
+  p.vec = mat.or.vec(nr=length(vec.x)-1, nc=1)
+  for(i in 1:length(vec.x)-1){
+    p.vec[i] = 100*(vec.x[i+1] - vec.x[i])/vec.x[i]
+  }
+  p.vec
+}
+
+for(i in 1:16){
+  p.change.1 = per.chg(as.numeric(soilMoist_yearly[i, 2:30]))
+  if(i == 1){
+    plot(p.change.1, type='l', ylim=c(-1.5,1.5))
+  }else{  
+    lines(p.change.1, col=i)
+  }
+  abline(h=0, col='black', lty=2)
+}
+
+for(i in 1:16){
+  p.change.1 = per.chg(as.numeric(soilMoist_yearly[i, 3:30]))
+  
+  png(paste("figures/spinup_clim_figures/soilMoist_percent_chg_", soilMoist_yearly[i,1], ".png", sep=""))
+  plot(p.change.1, type='l', ylim=c(-1.5,1.5), 
+       main=paste(soilMoist_yearly[i,1], "soilMoist"), ylab = c("percent change"), xlab = c("Years from start"))
+  abline(h=0, col='grey')
+  #abline(lm(p.change.1~seq(1:27)), col='red', lty=2)
+  lines(rollmean(p.change.1, 5) ~ seq(5,27), col='blue', lwd=2)
+  legend("topleft", legend=c("% change from previous year", "5-year rolling mean % chg"), 
+         lty=c(1,1), col=c('black', 'blue'), lwd=c(1,2))
+  dev.off()
+}
 
 
-basin.agg = lapply(vars.stor, function(var) extract_ts(raster.path = file.path(path.base, "yearly", "var"), 
-                                                       shp = basins, 
-                                                       years = seq(1981, 2010), 
-                                                       var, 
-                                                       row.nm = as.character(basins$name),
-                                                       out.nm = paste(path.out, var, "/basin_", var, "_km3_daily.csv", sep=""),
-                                                       check.file = 0))
+# grndWater_mm_pgi
+var="grndWater_mm_pgi"
+grndWater_pgi_yearly = 
+  extract_ts(raster.path = file.path(path.base, "yearly", "grndWater_mm_pgi"), 
+             shp = basins, 
+             years = seq(1981, 2009), 
+             var = "grndWater_mm_pgi", 
+             row.nm = as.character(basins$name),
+             out.nm = paste(path.out, var, "/basin_", var, "_km3_yearly.csv", sep=""),
+             check.file = 0)
+
+for(i in 1:16){
+  png(paste("figures/spinup_clim_figures/grndWater_pgi_avekm3Yr_", grndWater_pgi_yearly[i,1], ".png", sep=""))
+  plot(as.numeric(grndWater_pgi_yearly[i,2:29]), type='l',
+       main=paste(grndWater_pgi_yearly[i,1], "ave grndWater_pgi"), ylab = c("km3/yr"), xlab = c("Years from start"))
+  dev.off()
+}
+
+ymin = min(as.numeric(grndWater_pgi_yearly[,2:29]))
+ymax = max(as.numeric(grndWater_pgi_yearly[,2:29]))
+
+for(i in 1:16){
+  if(i==1){
+    plot(as.numeric(grndWater_pgi_yearly[i,2:29]), type='l', ylim = c(ymin, ymax))
+  }
+  lines(as.numeric(grndWater_pgi_yearly[i,2:29]), col=i)
+}
+
+# percent change
+for(i in 1:16){
+  p.change.1 = per.chg(as.numeric(grndWater_pgi_yearly[i, 3:30]))
+  
+  png(paste("figures/spinup_clim_figures/grndWater_pgi_percent_chg_", grndWater_pgi_yearly[i,1], ".png", sep=""))
+  plot(p.change.1, type='l', ylim=c(-1.5,1.5), 
+       main=paste(grndWater_pgi_yearly[i,1], "grndWater_pgi"), ylab = c("percent change"), xlab = c("Years from start"))
+  abline(h=0, col='grey')
+  #abline(lm(p.change.1~seq(1:27)), col='red', lty=2)
+  lines(rollmean(p.change.1, 5) ~ seq(5,27), col='blue', lwd=2)
+  legend("topleft", legend=c("% change from previous year", "5-year rolling mean % chg"), 
+         lty=c(1,1), col=c('black', 'blue'), lwd=c(1,2))
+  dev.off()
+}
+
+
+######################################################################################################################################
 
 ######################################################################################################################################
